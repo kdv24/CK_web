59c0206c570cfd15c84c5acea1805eeff0dc7df8
Kelly de Vries <kdv@me.com>
2015-02-09 20:22:26 -0800

    recovering index file.  ugh

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+<?php include('inc/header.php'); ?>
+
+      <div id="storefront">
+            <img src="img/yarnwall.jpg" alt="CloseKnit Storefront">
+      </div> 
+<?php include('inc/footer.php'); ?>
+	
+</body>
+</html>
